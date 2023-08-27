7b53c4dce03b234716f56d73c01be80b2ec78e92
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-27 11:14:29 -0500

    Add state management to app with improved styling for cards.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react"
 import Cards from "./components/cards/Cards"
 import Form from "./components/form/Form"
 
 export default function App() {
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+  const [month, setMonth] = useState('')
+  const [year, setYear] = useState('')
+  const [cvc, setCvc] = useState('')
+
   return (
     <>
-      <Cards />
-      <Form />
+      <Cards
+        name={name}
+        number={number}
+        month={month}
+        year={year}
+        cvc={cvc}
+      />
+
+      <Form
+        name={name}
+        setName={setName}
+        number={number}
+        setNumber={setNumber}
+        month={month}
+        setMonth={setMonth}
+        year={year}
+        setYear={setYear}
+        cvc={cvc}
+        setCvc={setCvc}
+      />
     </>
   )
 }
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,5 +1,7 @@
 import backgroundLarge from '../../assets/bg-main-desktop.png'
 import backgroundSmall from '../../assets/bg-main-mobile.png'
+import CardFront from '../../assets/bg-card-front.png'
+import CardBack from '../../assets/bg-card-back.png'
 import { useMediaQuery } from "usehooks-ts"
 import styles from './cards.module.css'
 
@@ -11,6 +13,25 @@ export default function Cards() {
             className={styles.card_container}
         >
             <img src={largeScreen ? backgroundLarge : backgroundSmall} alt="" />
+
+            <div
+                className={styles.cards}
+            >
+
+                <div>
+
+                    <img src={CardBack} alt='' />
+
+                </div>
+
+                <div>
+
+                    <img src={CardFront} alt='' />
+
+                </div>
+
+            </div>
+
         </div>
     )
 }
